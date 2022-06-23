d8886fa9f6d5f195ee73fb118f1160bc09c356a4
jordy.jordy <jordy.jordy@gojek.com>
2022-06-23 17:14:57 +0700

    add convenience store payment request builder

--- corekit/src/main/kotlin/com/midtrans/sdk/corekit/api/requestbuilder/ConvenienceStorePaymentRequestBuilder.kt
+++ corekit/src/main/kotlin/com/midtrans/sdk/corekit/api/requestbuilder/ConvenienceStorePaymentRequestBuilder.kt
@@ -4,16 +4,17 @@ import com.midtrans.sdk.corekit.api.exception.InvalidPaymentTypeException
 import com.midtrans.sdk.corekit.api.model.PaymentType
 import com.midtrans.sdk.corekit.internal.network.model.request.PaymentRequest
 
-class PayLaterPaymentRequestBuilder : PaymentRequestBuilder() {
+class ConvenienceStorePaymentRequestBuilder : PaymentRequestBuilder() {
     private lateinit var paymentType: String
 
-    fun withPaymentType(@PaymentType.Def value: String): PayLaterPaymentRequestBuilder = apply {
+    fun withPaymentType(@PaymentType.Def value: String): ConvenienceStorePaymentRequestBuilder = apply {
         paymentType = value
     }
 
     override fun build(): PaymentRequest {
-        return when (paymentType) {
-            PaymentType.AKULAKU -> PaymentRequest(paymentType = paymentType)
+        return when(paymentType) {
+            PaymentType.ALFAMART,
+            PaymentType.INDOMARET -> PaymentRequest(paymentType = paymentType)
             else -> throw InvalidPaymentTypeException()
         }
     }
