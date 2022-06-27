3429d37a95ee1987d9697aff90f5b352928b95e7
Muhammad Fauzi Masykur <muhammadfauzimasykur@gmail.com>
2022-06-28 01:45:59 +0700

    update demo app to handle promo and two click

--- app/src/main/kotlin/com/midtrans/sdk/sample/SampleActivity.kt
+++ app/src/main/kotlin/com/midtrans/sdk/sample/SampleActivity.kt
@@ -1,6 +1,9 @@
 package com.midtrans.sdk.sample
 
 import android.os.Bundle
+import android.view.ViewGroup
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.Column
@@ -8,9 +11,12 @@ import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
 import androidx.compose.runtime.*
+import androidx.compose.ui.UiComposable
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.viewinterop.AndroidView
 import androidx.lifecycle.ViewModelProvider
 import com.midtrans.sdk.corekit.SnapCore
 
@@ -31,6 +37,7 @@ class SampleActivity : AppCompatActivity() {
     @Preview
     fun Greeting() {
         Column {
+            val uriHandler = LocalUriHandler.current
             var text by remember { mutableStateOf("") }
             Text("insert snap token", style = TextStyle(color = Color.Red))
             TextField(value = text, onValueChange = {
@@ -38,20 +45,29 @@ class SampleActivity : AppCompatActivity() {
             }, enabled = true, readOnly = false)
 
             Button(onClick = {
-                viewModel.chargeUsingCreditCard(text)
+                viewModel.chargeUsingCreditCardWithPromo(text)
             }) {
-
+                Text(text = "Charge")
             }
+
+            Button(onClick = {
+                viewModel.getTwoClickToken()
+            }) {
+                Text(text = "2click card token")
+            }
+
             Button(onClick = {
                 viewModel.getCardTokenBasic()
             }) {
-                Text(text = "Create card token")
+                Text(text = "normal card token")
             }
+
             Button(onClick = {
                 viewModel.deleteSavedCard()
             }) {
                 Text(text = "Delete card token")
             }
+
         }
     }
 }
