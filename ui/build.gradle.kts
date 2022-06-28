501037244228eb6872c50e3a56e239e7abb870d3
jordy.jordy <jordy.jordy@gojek.com>
2022-06-28 18:19:53 +0700

    enable compose in ui gradle

--- ui/build.gradle.kts
+++ ui/build.gradle.kts
@@ -23,10 +23,20 @@ android {
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+
+    buildFeatures {
+        compose = true
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = com.gtf.snap.Dependencies.composeVersion
+    }
+
     kotlinOptions {
         jvmTarget = "1.8"
     }
