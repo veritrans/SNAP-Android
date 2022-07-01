99494fde24942cedb523e050e9342d77fda4d197
Wahyu <wahyu@wahyucorp.com>
2022-07-01 14:21:45 +0700

    wip app bar

--- ui/src/main/kotlin/com/midtrans/sdk/uikit/internal/view/SnapAppBar.kt
+++ ui/src/main/kotlin/com/midtrans/sdk/uikit/internal/view/SnapAppBar.kt
@@ -2,15 +2,18 @@ package com.midtrans.sdk.uikit.internal.view
 
 import androidx.annotation.DrawableRes
 import androidx.annotation.IntegerRes
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 
 object SnapAppBar {
 }
@@ -23,11 +26,14 @@ fun SnapAppBar(
     onClick: () -> Unit
 ){
     Row (verticalAlignment = Alignment.CenterVertically,
-    horizontalArrangement = Arrangement.Center){
-        IconButton(onClick = onClick) {
+    horizontalArrangement = Arrangement.Start,
+    modifier = Modifier.height(64.dp).background(color = Color.Cyan).fillMaxWidth(fraction = 1.0f)){
+        IconButton(onClick = onClick,
+        modifier = Modifier.mar) {
             Icon(
                 painter = painterResource(id = iconResId),
-                contentDescription = null
+                tint = Color.Unspecified,
+                contentDescription = null,
             )
         }
 
