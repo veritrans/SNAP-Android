be21fa2b75d326a9b1cedaba5762aa255f578a81
Muhammad Fauzi Masykur <muhammadfauzimasykur@gmail.com>
2022-07-13 14:32:24 +0700

    fix disctounted gross amount on promo detail to double

--- corekit/src/main/kotlin/com/midtrans/sdk/corekit/internal/network/model/request/PromoDetailRequest.kt
+++ corekit/src/main/kotlin/com/midtrans/sdk/corekit/internal/network/model/request/PromoDetailRequest.kt
@@ -1,6 +1,6 @@
 package com.midtrans.sdk.corekit.internal.network.model.request
 
 data class PromoDetailRequest(
-    val discountedGrossAmount: String? = null,
+    val discountedGrossAmount: Double? = null,
     val promoId: String? = null,
 )
