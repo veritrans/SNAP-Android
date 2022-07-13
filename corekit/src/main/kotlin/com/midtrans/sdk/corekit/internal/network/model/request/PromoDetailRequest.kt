59b966b0bb867453560aaffe9c2700bb1c60c657
Muhammad Fauzi Masykur <muhammadfauzimasykur@gmail.com>
2022-07-13 13:58:28 +0700

    update promo detail request to be accessible to use as function param

--- corekit/src/main/kotlin/com/midtrans/sdk/corekit/internal/network/model/request/PromoDetailRequest.kt
+++ corekit/src/main/kotlin/com/midtrans/sdk/corekit/internal/network/model/request/PromoDetailRequest.kt
@@ -1,6 +1,6 @@
 package com.midtrans.sdk.corekit.internal.network.model.request
 
-internal data class PromoDetailRequest(
+data class PromoDetailRequest(
     val discountedGrossAmount: String? = null,
     val promoId: String? = null,
 )
