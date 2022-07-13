b11c8711b0c3a4a365661dbf62abf684d8c969d3
Muhammad Fauzi Masykur <muhammadfauzimasykur@gmail.com>
2022-07-14 01:32:54 +0700

    add unit tests

--- corekit/src/test/java/com/midtrans/sdk/corekit/api/requestbuilder/OneClickCardPaymentRequestBuilderTest.kt
+++ corekit/src/test/java/com/midtrans/sdk/corekit/api/requestbuilder/OneClickCardPaymentRequestBuilderTest.kt
@@ -2,56 +2,56 @@ package com.midtrans.sdk.corekit.api.requestbuilder
 
 import com.midtrans.sdk.corekit.api.exception.InvalidPaymentTypeException
 import com.midtrans.sdk.corekit.api.model.PaymentType
-import com.midtrans.sdk.corekit.api.requestbuilder.payment.CreditCardPaymentRequestBuilder
+import com.midtrans.sdk.corekit.api.requestbuilder.payment.OneClickCardPaymentRequestBuilder
 import com.midtrans.sdk.corekit.internal.network.model.request.PromoDetailRequest
 import org.junit.Assert
 import org.junit.Test
 
-internal class CreditCardPaymentRequestBuilderTest {
-
-    private val cardToken = "cardToken"
+internal class OneClickCardPaymentRequestBuilderTest{
+    private val maskedCard = "maskedCard"
     private val installment = "offline_3"
     private val promo = PromoDetailRequest(discountedGrossAmount = 145000.0, promoId = "431" )
 
     @Test
     fun shouldConstructCreditCardPaymentRequestBasic() {
-        val request = CreditCardPaymentRequestBuilder()
+        val request = OneClickCardPaymentRequestBuilder()
             .withPaymentType(PaymentType.CREDIT_CARD)
-            .withCardToken(cardToken)
+            .withMaskedCard(maskedCard)
             .build()
         Assert.assertEquals(PaymentType.CREDIT_CARD, request.paymentType)
-        Assert.assertEquals(cardToken, request.paymentParams?.cardToken)
+        Assert.assertEquals(maskedCard, request.paymentParams?.maskedCard)
     }
 
     @Test
     fun shouldConstructCreditCardPaymentRequestWithInstallment() {
-        val request = CreditCardPaymentRequestBuilder()
+        val request = OneClickCardPaymentRequestBuilder()
             .withPaymentType(PaymentType.CREDIT_CARD)
-            .withCardToken(cardToken)
+            .withMaskedCard(maskedCard)
             .withInstallment(installment)
             .build()
         Assert.assertEquals(PaymentType.CREDIT_CARD, request.paymentType)
-        Assert.assertEquals(cardToken, request.paymentParams?.cardToken)
+        Assert.assertEquals(maskedCard, request.paymentParams?.maskedCard)
         Assert.assertEquals(installment, request.paymentParams?.installment)
     }
 
     @Test
     fun shouldConstructCreditCardPaymentRequestWithPromo() {
-        val request = CreditCardPaymentRequestBuilder()
+        val request = OneClickCardPaymentRequestBuilder()
             .withPaymentType(PaymentType.CREDIT_CARD)
-            .withCardToken(cardToken)
+            .withMaskedCard(maskedCard)
             .withPromo(promo)
             .build()
         Assert.assertEquals(PaymentType.CREDIT_CARD, request.paymentType)
-        Assert.assertEquals(cardToken, request.paymentParams?.cardToken)
-        Assert.assertEquals(promo, request.promoDetails)
+        Assert.assertEquals(maskedCard, request.paymentParams?.maskedCard)
+        Assert.assertEquals(promo.discountedGrossAmount, request.promoDetails?.discountedGrossAmount)
+        Assert.assertEquals(promo.promoId, request.promoDetails?.promoId)
     }
 
     @Test
     fun shouldThrowUninitializedPropertyAccessExceptionWhenCardTokenIsNotInitialize() {
         Assert.assertThrows(
             UninitializedPropertyAccessException::class.java
-        ) { CreditCardPaymentRequestBuilder()
+        ) { OneClickCardPaymentRequestBuilder()
             .withPaymentType(PaymentType.CREDIT_CARD)
             .build() }
     }
@@ -60,9 +60,9 @@ internal class CreditCardPaymentRequestBuilderTest {
     fun shouldThrowInvalidPaymentTypeExceptionWhenPaymentTypeIsNotSupported() {
         Assert.assertThrows(
             InvalidPaymentTypeException::class.java
-        ) { CreditCardPaymentRequestBuilder()
+        ) { OneClickCardPaymentRequestBuilder()
             .withPaymentType(PaymentType.KLIK_BCA)
-            .withCardToken(cardToken)
+            .withMaskedCard(maskedCard)
             .build() }
     }
 }
