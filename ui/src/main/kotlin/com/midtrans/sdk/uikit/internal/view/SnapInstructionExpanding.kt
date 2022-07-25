6ecd5e72bc994c9f9750a23b9e694ff00798ee6a
jordy.jordy <jordy.jordy@gojek.com>
2022-07-25 22:14:15 +0700

    tidy up snap instruction

--- ui/src/main/kotlin/com/midtrans/sdk/uikit/internal/view/SnapInstructionExpanding.kt
+++ ui/src/main/kotlin/com/midtrans/sdk/uikit/internal/view/SnapInstructionExpanding.kt
@@ -4,19 +4,17 @@ import androidx.annotation.DrawableRes
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.expandVertically
 import androidx.compose.animation.shrinkVertically
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.midtrans.sdk.uikit.R
+import com.midtrans.sdk.uikit.internal.view.SnapColors.LINK
 
 @Composable
 fun SnapInstructionExpanding(
@@ -26,25 +24,28 @@ fun SnapInstructionExpanding(
     onExpandClick: (() -> Unit),
     expandingContent: @Composable (() -> Unit)? = null
 ) {
-    var isExpanding by remember { mutableStateOf(false) }
 
     Column(verticalArrangement = Arrangement.spacedBy(20.dp)) {
         Row(
             verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.spacedBy(13.5.dp),
+            horizontalArrangement = Arrangement.spacedBy(13.dp),
             modifier = Modifier.height(19.dp)
         ) {
             Row(
+                verticalAlignment = Alignment.CenterVertically,
                 horizontalArrangement = Arrangement.spacedBy(8.dp),
-                modifier = Modifier
-                    .height(19.dp)
             ) {
                 Icon(
                     painter = painterResource(id = iconResId),
-                    contentDescription = null
+                    contentDescription = null,
+                    tint = SnapColors.getARGBColor(LINK),
+                    modifier = Modifier
+                        .height(16.dp)
+                        .width(16.dp)
                 )
                 Text(
                     text = title,
+                    color = SnapColors.getARGBColor(LINK),
                     style = SnapTypography.STYLES.snapTextMediumMedium
                 )
             }
@@ -53,7 +54,8 @@ fun SnapInstructionExpanding(
             }) {
                 Icon(
                     painter = painterResource(id = R.drawable.ic_double_caret_vertical),
-                    contentDescription = null
+                    contentDescription = null,
+                    tint = SnapColors.getARGBColor(LINK)
                 )
             }
         }
