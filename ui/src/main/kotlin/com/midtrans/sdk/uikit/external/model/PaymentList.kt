f6d2208a39babb92c8495fa6260df9b17d0663ce
andre.tobing <andre.tobing@gojek.com>
2022-08-08 11:26:20 +0700

    Change parcelize plugin

--- ui/src/main/kotlin/com/midtrans/sdk/uikit/external/model/PaymentList.kt
+++ ui/src/main/kotlin/com/midtrans/sdk/uikit/external/model/PaymentList.kt
@@ -1,7 +1,7 @@
 package com.midtrans.sdk.uikit.external.model
 
 import android.os.Parcelable
-import kotlinx.android.parcel.Parcelize
+import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class PaymentList(
--- ui/src/main/kotlin/com/midtrans/sdk/uikit/internal/model/CustomerInfo.kt
+++ ui/src/main/kotlin/com/midtrans/sdk/uikit/internal/model/CustomerInfo.kt
@@ -1,7 +1,7 @@
 package com.midtrans.sdk.uikit.internal.model
 
 import android.os.Parcelable
-import kotlinx.android.parcel.Parcelize
+import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class CustomerInfo(
