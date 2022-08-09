0bc29117dbf1a988f7f0dd2f4da66cecb27cfde4
andre.tobing <andre.tobing@gojek.com>
2022-08-09 13:55:05 +0700

    add getCustomerInfo testcases

--- ui/src/test/java/com/midtrans/sdk/uikit/internal/presentation/paymentoption/PaymentOptionViewModelTest.kt
+++ ui/src/test/java/com/midtrans/sdk/uikit/internal/presentation/paymentoption/PaymentOptionViewModelTest.kt
@@ -1,367 +1,166 @@
 package com.midtrans.sdk.uikit.internal.presentation.paymentoption
 
+import com.midtrans.sdk.corekit.api.model.Address
+import com.midtrans.sdk.corekit.api.model.CustomerDetails
 import com.midtrans.sdk.uikit.external.model.PaymentList
 import com.midtrans.sdk.uikit.external.model.PaymentMethod
 import org.hamcrest.MatcherAssert.assertThat
 import org.hamcrest.Matchers.*
-import org.junit.Assert.assertTrue
+import org.junit.Assert.*
+import org.junit.Before
 import org.junit.Test
 
-//TODO will be fixed after PR, need to PR fast for integration
 internal class PaymentOptionViewModelTest {
+    private lateinit var viewModel: PaymentOptionViewModel
 
-    /*@Test
-    fun whenBankTransferMethodIsInPaymentListInitiateListShouldCreateBankTransferList() {
-        val list = PaymentList(
-            options = listOf(
-                PaymentMethod(
-                    type = "permata_va",
-                    category = "bank_transfer",
-                    status = "up"
-                ),
-                PaymentMethod(
-                    type = "bca_va",
-                    category = "bank_transfer",
-                    status = "up"
-                ),
-                PaymentMethod(
-                    type = "bni_va",
-                    category = "bank_transfer",
-                    status = "up"
-                )
-            )
-        )
-        val vm = PaymentOptionViewModel()
-        val result = vm.initiateList(list, true)
-        assertTrue(result.paymentMethods.size == 1)
-        assertThat(
-            result.paymentMethods[0],
-            allOf(
-                hasProperty("type", equalTo("bank_transfer")),
-                hasProperty("methods", equalTo(listOf("permata_va", "bca_va", "bni_va"))),
-                hasProperty("icons", hasSize<List<Int>>(3))
-            )
-        )
+    @Before
+    fun setUp() {
+        viewModel = PaymentOptionViewModel()
     }
 
     @Test
-    fun whenBankTransferMethodIncludeOtherVaInitiateListShouldCreateBankTransferListWithAllBankIcon() {
-        val list = PaymentList(
-            options = listOf(
-                PaymentMethod(
-                    type = "permata_va",
-                    category = "bank_transfer",
-                    status = "up"
-                ),
-                PaymentMethod(
-                    type = "other_va",
-                    category = "bank_transfer",
-                    status = "up"
-                )
-            )
-        )
-        val vm = PaymentOptionViewModel()
-        val result = vm.initiateList(list, true)
-        assertTrue(result.paymentMethods.size == 1)
-        assertThat(
-            result.paymentMethods[0],
-            allOf(
-                hasProperty("type", equalTo("bank_transfer")),
-                hasProperty("methods", equalTo(listOf("permata_va", "other_va"))),
-                hasProperty("icons", hasSize<List<Int>>(5))
-            )
-        )
+    fun getCustomerInfoWhenInputNullShouldReturnNull() {
+        val result = viewModel.getCustomerInfo(null)
+        assertNull(result)
     }
 
     @Test
-    fun whenSoloPaymentMethodsAreInPaymentListInitiateListShouldCreateThoseMethodsList() {
-        val list = PaymentList(
-            options = listOf(
-                PaymentMethod(
-                    type = "bca_klikbca",
-                    status = "up"
-                ),
-                PaymentMethod(
-                    type = "bca_klikpay",
-                    status = "up"
-                ),
-                PaymentMethod(
-                    type = "cimb_clicks",
-                    status = "up"
-                ),
-                PaymentMethod(
-                    type = "bri_epay",
-                    status = "up"
-                ),
-                PaymentMethod(
-                    type = "danamon_online",
-                    status = "up"
-                ),
-                PaymentMethod(
-                    type = "gopay",
-                    status = "up"
-                ),
-                PaymentMethod(
-                    type = "akulaku",
-                    status = "up"
-                ),
-                PaymentMethod(
-                    type = "uob_ezpay",
-                    mode = listOf("uobweb", "uobapp"),
-                    status = "up"
-                )
+    fun getCustomerInfoWhenFirstNameOnlyShouldPutFirstNameAsName() {
+        val result = viewModel.getCustomerInfo(
+            CustomerDetails(
+                firstName = "first name"
             )
         )
-        val vm = PaymentOptionViewModel()
-        val result = vm.initiateList(list, true)
-        assertTrue(result.paymentMethods.size == 8)
-        assertThat(
-            result.paymentMethods[0],
-            allOf(
-                hasProperty("type", equalTo("bca_klikbca")),
-                hasProperty("icons", hasSize<List<Int>>(1))
-            )
-        )
-        assertThat(
-            result.paymentMethods[1],
-            allOf(
-                hasProperty("type", equalTo("bca_klikpay")),
-                hasProperty("icons", hasSize<List<Int>>(1))
-            )
-        )
-        assertThat(
-            result.paymentMethods[2],
-            allOf(
-                hasProperty("type", equalTo("cimb_clicks")),
-                hasProperty("icons", hasSize<List<Int>>(1))
-            )
-        )
-        assertThat(
-            result.paymentMethods[3],
-            allOf(
-                hasProperty("type", equalTo("bri_epay")),
-                hasProperty("icons", hasSize<List<Int>>(1))
-            )
-        )
-        assertThat(
-            result.paymentMethods[4],
-            allOf(
-                hasProperty("type", equalTo("danamon_online")),
-                hasProperty("icons", hasSize<List<Int>>(1))
-            )
-        )
-        assertThat(
-            result.paymentMethods[5],
-            allOf(
-                hasProperty("type", equalTo("gopay")),
-                hasProperty("icons", hasSize<List<Int>>(2))
-            )
-        )
-        assertThat(
-            result.paymentMethods[6],
-            allOf(
-                hasProperty("type", equalTo("akulaku")),
-                hasProperty("icons", hasSize<List<Int>>(1))
-            )
-        )
-        assertThat(
-            result.paymentMethods[7],
-            allOf(
-                hasProperty("type", equalTo("uob_ezpay")),
-                hasProperty("methods", equalTo(listOf("uobweb", "uobapp"))),
-                hasProperty("icons", hasSize<List<Int>>(1))
-            )
-        )
+        assertNotNull(result)
+        assertEquals("first name", result?.name)
     }
 
     @Test
-    fun whenConvenienceStorePaymentMethodIsInTheListInitiateListShouldCreateConvenienceStoreList() {
-        val list = PaymentList(
-            options = listOf(
-                PaymentMethod(
-                    type = "indomaret",
-                    category = "cstore",
-                    status = "up"
-                ),
-                PaymentMethod(
-                    type = "alfamart",
-                    category = "cstore",
-                    status = "up"
-                )
+    fun getCustomerInfoWhenLastNameOnlyShouldPutLastNameAsName() {
+        val result = viewModel.getCustomerInfo(
+            CustomerDetails(
+                lastName = "last name"
             )
         )
-        val vm = PaymentOptionViewModel()
-        val result = vm.initiateList(list, true)
-        assertTrue(result.paymentMethods.size == 2)
-        assertThat(
-            result.paymentMethods[0],
-            allOf(
-                hasProperty("type", equalTo("indomaret")),
-                hasProperty("icons", hasSize<List<Int>>(1))
-            )
-        )
-        assertThat(
-            result.paymentMethods[1],
-            allOf(
-                hasProperty("type", equalTo("alfamart")),
-                hasProperty("icons", hasSize<List<Int>>(3))
-            )
-        )
+        assertNotNull(result)
+        assertEquals("last name", result?.name)
     }
 
     @Test
-    fun whenShopeepayForPhoneAndDeviceIsPhoneInitiateListShouldCreateListShopeepay() {
-        val list = PaymentList(
-            options = listOf(
-                PaymentMethod(
-                    type = "shopeepay",
-                    status = "up"
-                )
+    fun getCustomerInfoWhenFirstNameAndLastNameShouldPutBothAsName() {
+        val result = viewModel.getCustomerInfo(
+            CustomerDetails(
+                firstName = "first name",
+                lastName = "last name"
             )
         )
-        val vm = PaymentOptionViewModel()
-        val result = vm.initiateList(list, false)
-        assertTrue(result.paymentMethods.size == 1)
-        assertThat(
-            result.paymentMethods[0],
-            allOf(
-                hasProperty("type", equalTo("shopeepay")),
-                hasProperty("icons", hasSize<List<Int>>(2))
-            )
-        )
+        assertNotNull(result)
+        assertEquals("first name last name", result?.name)
     }
 
     @Test
-    fun whenShopeepayForPhoneAndDeviceIsTabletInitiateListShouldNotCreateList() {
-        val list = PaymentList(
-            options = listOf(
-                PaymentMethod(
-                    type = "shopeepay",
-                    status = "up"
+    fun getCustomerInfoWhenShippingAddressIsProvidedShouldCreateAddressLines() {
+        var result = viewModel.getCustomerInfo(
+            CustomerDetails(
+                shippingAddress = Address(
+                    address = "street name",
+                    city = "city name",
+                    postalCode = "postal code"
                 )
             )
         )
-        val vm = PaymentOptionViewModel()
-        val result = vm.initiateList(list, true)
-        assertTrue(result.paymentMethods.isEmpty())
+        assertNotNull(result)
+        assertThat(
+            result!!.addressLines, hasItems("street name", "city name", "postal code")
+        )
+        assertTrue(result.addressLines.size == 3)
+
+        result = viewModel.getCustomerInfo(
+            CustomerDetails(
+                shippingAddress = Address(
+                    city = "city name",
+                    postalCode = "postal code"
+                )
+            )
+        )
+        assertNotNull(result)
+        assertThat(
+            result!!.addressLines, hasItems("city name", "postal code")
+        )
+        assertTrue(result.addressLines.size == 2)
+
+        result = viewModel.getCustomerInfo(
+            CustomerDetails(
+                shippingAddress = Address(
+                    postalCode = "postal code"
+                )
+            )
+        )
+        assertNotNull(result)
+        assertThat(
+            result!!.addressLines, hasItems("postal code")
+        )
+        assertTrue(result.addressLines.size == 1)
     }
 
     @Test
-    fun whenShopeepayForTabletAndDeviceIsTabletInitiateListShouldCreateList() {
-        val list = PaymentList(
-            options = listOf(
-                PaymentMethod(
-                    type = "qris",
-                    acquirer = "shopeepay",
-                    status = "up"
+    fun getCustomerInfoWhenBillingAddressIsProvidedShouldCreateAddressLines() {
+        var result = viewModel.getCustomerInfo(
+            CustomerDetails(
+                billingAddress = Address(
+                    address = "street name",
+                    city = "city name",
+                    postalCode = "postal code"
                 )
             )
         )
-        val vm = PaymentOptionViewModel()
-        val result = vm.initiateList(list, true)
-        assertTrue(result.paymentMethods.size == 1)
+        assertNotNull(result)
         assertThat(
-            result.paymentMethods[0],
-            allOf(
-                hasProperty("type", equalTo("shopeepay")),
-                hasProperty("icons", hasSize<List<Int>>(2))
+            result!!.addressLines, hasItems("street name", "city name", "postal code")
+        )
+        assertTrue(result.addressLines.size == 3)
+
+        result = viewModel.getCustomerInfo(
+            CustomerDetails(
+                shippingAddress = Address(
+                    city = "city name",
+                    postalCode = "postal code"
+                )
             )
         )
+        assertNotNull(result)
+        assertThat(
+            result!!.addressLines, hasItems("city name", "postal code")
+        )
+        assertTrue(result.addressLines.size == 2)
+
+        result = viewModel.getCustomerInfo(
+            CustomerDetails(
+                shippingAddress = Address(
+                    postalCode = "postal code"
+                )
+            )
+        )
+        assertNotNull(result)
+        assertThat(
+            result!!.addressLines, hasItems("postal code")
+        )
+        assertTrue(result.addressLines.size == 1)
     }
 
     @Test
-    fun whenShopeepayForTabletAndDeviceIsPhoneInitiateListShouldNotCreateList() {
-        val list = PaymentList(
-            options = listOf(
-                PaymentMethod(
-                    type = "qris",
-                    acquirer = "shopeepay",
-                    status = "up"
-                )
-            )
-        )
-        val vm = PaymentOptionViewModel()
-        val result = vm.initiateList(list, false)
-        assertTrue(result.paymentMethods.isEmpty())
+    fun getCustomerInfoWhenNoAddressIsProvidedShouldReturnEmptyList() {
+        val result = viewModel.getCustomerInfo(CustomerDetails())
+        assertNotNull(result)
+        assertTrue(result!!.addressLines.isEmpty())
     }
 
     @Test
-    fun initiateListShouldOnlyCreateListForPaymentWithStatusUp() {
-        val list = PaymentList(
-            options = listOf(
-                PaymentMethod(
-                    type = "bca_klikbca",
-                    status = "up"
-                ),
-                PaymentMethod(
-                    type = "bca_klikpay",
-                    status = "up"
-                ),
-                PaymentMethod(
-                    type = "cimb_clicks",
-                    status = "down"
-                ),
-                PaymentMethod(
-                    type = "bri_epay",
-                    status = "up"
-                ),
-                PaymentMethod(
-                    type = "danamon_online",
-                    status = "down"
-                ),
-                PaymentMethod(
-                    type = "gopay",
-                    status = "up"
-                ),
-                PaymentMethod(
-                    type = "akulaku",
-                    status = "down"
-                ),
-                PaymentMethod(
-                    type = "uob_ezpay",
-                    mode = listOf("uobweb", "uobapp"),
-                    status = "up"
-                )
-            )
-        )
-        val vm = PaymentOptionViewModel()
-        val result = vm.initiateList(list, true)
-        assertTrue(result.paymentMethods.size == 5)
-        assertThat(
-            result.paymentMethods[0],
-            allOf(
-                hasProperty("type", equalTo("bca_klikbca")),
-                hasProperty("icons", hasSize<List<Int>>(1))
-            )
-        )
-        assertThat(
-            result.paymentMethods[1],
-            allOf(
-                hasProperty("type", equalTo("bca_klikpay")),
-                hasProperty("icons", hasSize<List<Int>>(1))
-            )
-        )
-        assertThat(
-            result.paymentMethods[2],
-            allOf(
-                hasProperty("type", equalTo("bri_epay")),
-                hasProperty("icons", hasSize<List<Int>>(1))
-            )
-        )
-        assertThat(
-            result.paymentMethods[3],
-            allOf(
-                hasProperty("type", equalTo("gopay")),
-                hasProperty("icons", hasSize<List<Int>>(2))
-            )
-        )
-        assertThat(
-            result.paymentMethods[4],
-            allOf(
-                hasProperty("type", equalTo("uob_ezpay")),
-                hasProperty("methods", equalTo(listOf("uobweb", "uobapp"))),
-                hasProperty("icons", hasSize<List<Int>>(1))
-            )
-        )
-    }*/
+    fun getCustomerInfoWhenPhoneNumberIsProvidedShouldReturnPhoneNumber() {
+        val result = viewModel.getCustomerInfo(CustomerDetails(
+            phone = "0888887777777"
+        ))
+        assertNotNull(result)
+        assertEquals("0888887777777", result!!.phone)
+    }
 }
