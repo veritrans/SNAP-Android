1db798843b81d704c57f95eee9d1ac0ec5444eb7
jordy.jordy <jordy.jordy@gojek.com>
2022-08-09 16:00:01 +0700

    update pay method to use snap core

--- ui/src/main/kotlin/com/midtrans/sdk/uikit/internal/presentation/directdebit/DirectDebitViewModel.kt
+++ ui/src/main/kotlin/com/midtrans/sdk/uikit/internal/presentation/directdebit/DirectDebitViewModel.kt
@@ -1,9 +1,36 @@
 package com.midtrans.sdk.uikit.internal.presentation.directdebit
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import com.midtrans.sdk.corekit.SnapCore
+import com.midtrans.sdk.corekit.api.callback.Callback
+import com.midtrans.sdk.corekit.api.exception.SnapError
+import com.midtrans.sdk.corekit.api.model.TransactionResponse
+import com.midtrans.sdk.corekit.api.requestbuilder.payment.DirectDebitPaymentRequestBuilder
 
 class DirectDebitViewModel : ViewModel() {
-    fun pay(paymentType: String, userId: String?) {
-        //TODO pay
+
+    fun pay(
+        snapToken: String,
+        paymentType: String,
+        userId: String?
+    ) {
+        val builder = DirectDebitPaymentRequestBuilder()
+            .withPaymentType(paymentType)
+            .withKlikBcaUserId(userId)
+
+        SnapCore.getInstance()?.pay(
+            snapToken = snapToken,
+            paymentRequestBuilder = builder,
+            callback = object : Callback<TransactionResponse> {
+                override fun onSuccess(result: TransactionResponse) {
+                    Log.d("DirectDebitPay", result.statusCode.orEmpty())
+                }
+
+                override fun onError(error: SnapError) {
+                    Log.e("DirectDebitPay", error.javaClass.name)
+                }
+            }
+        )
     }
 }
