eb5ec18ad5498b80249acae93e8b7b9415107e4c
jordy.jordy <jordy.jordy@gojek.com>
2022-08-12 01:03:05 +0700

    handle web view started

--- ui/src/main/kotlin/com/midtrans/sdk/uikit/internal/view/SnapWebView.kt
+++ ui/src/main/kotlin/com/midtrans/sdk/uikit/internal/view/SnapWebView.kt
@@ -1,14 +1,16 @@
 package com.midtrans.sdk.uikit.internal.view
 
+import android.graphics.Bitmap
 import android.util.Log
 import android.view.ViewGroup
+import android.webkit.WebSettings
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.viewinterop.AndroidView
-import com.midtrans.sdk.corekit.api.exception.SnapError
 import com.midtrans.sdk.corekit.api.model.PaymentType
+
 
 @Composable
 @Preview(showBackground = true)
@@ -16,9 +18,8 @@ fun WebViewPreview() {
     SnapWebView(
         paymentType = PaymentType.BCA_KLIKPAY,
         url = "https://www.geeksforgeeks.org/webview-in-android-using-jetpack-compose/",
-        onWebViewFinished = {
-            Log.d("SnapWebView", "Preview")
-        }
+        onWebViewStarted = { Log.d("SnapWebView", "Started") },
+        onWebViewFinished = { Log.d("SnapWebView", "Finish") }
     )
 }
 
@@ -26,6 +27,7 @@ fun WebViewPreview() {
 fun SnapWebView(
     @PaymentType.Def paymentType: String,
     url: String,
+    onWebViewStarted: () -> Unit,
     onWebViewFinished: () -> Unit
 ) {
     AndroidView(factory = {
@@ -35,6 +37,12 @@ fun SnapWebView(
                 ViewGroup.LayoutParams.MATCH_PARENT
             )
             webViewClient = object : WebViewClient() {
+                override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+                    super.onPageStarted(view, url, favicon)
+                    Log.d("SnapWebView", "onPageStarted visibility : ${view?.visibility}")
+                    onWebViewStarted.invoke()
+                }
+
                 override fun onPageFinished(view: WebView?, url: String?) {
                     super.onPageFinished(view, url)
                     Log.d("SnapWebView", "onPageFinished url : $url")
@@ -45,6 +53,18 @@ fun SnapWebView(
                     )
                 }
             }
+            settings.apply { //NOTE: following settings in old midtrans sdk
+                allowFileAccess = false
+                javaScriptEnabled = true //TODO check do we need to enable javascript
+                domStorageEnabled = true
+                loadWithOverviewMode = true
+                useWideViewPort = true
+                builtInZoomControls = true
+                displayZoomControls = false
+                mixedContentMode = WebSettings.MIXED_CONTENT_ALWAYS_ALLOW
+            }
+            setInitialScale(1)
+            resumeTimers()
             loadUrl(url)
         }
     }, update = {
@@ -52,46 +72,47 @@ fun SnapWebView(
     })
 }
 
-
 private inline fun finishWebView(
     paymentType: String,
     url: String,
     onWebViewFinished: () -> Unit
 ) {
     when (paymentType) {
+        PaymentType.KLIK_BCA -> containPattern(
+            url = url,
+            pattern = SnapWebViewClient.CALLBACK_KLIK_BCA,
+            onPatternContained = onWebViewFinished
+        )
         PaymentType.BCA_KLIKPAY -> containPattern(
             url = url,
             pattern = SnapWebViewClient.CALLBACK_BCA_KLIK_PAY,
-            onPatternMatched = onWebViewFinished
+            onPatternContained = onWebViewFinished
         )
         PaymentType.BRI_EPAY -> containPattern(
             url = url,
             pattern = SnapWebViewClient.CALLBACK_BRI_EPAY,
-            onPatternMatched = onWebViewFinished
+            onPatternContained = onWebViewFinished
         )
         PaymentType.CIMB_CLICKS -> containPattern(
             url = url,
             pattern = SnapWebViewClient.CALLBACK_CIMB_CLICKS,
-            onPatternMatched = onWebViewFinished
+            onPatternContained = onWebViewFinished
         )
         PaymentType.DANAMON_ONLINE -> containPattern(
             url = url,
             pattern = SnapWebViewClient.CALLBACK_DANAMON_ONLINE,
-            onPatternMatched = onWebViewFinished
+            onPatternContained = onWebViewFinished
         )
     }
 }
 
-
 private inline fun containPattern(
     url: String,
     pattern: String,
-    onPatternMatched: () -> Unit
+    onPatternContained: () -> Unit
 ) {
     if (url.contains(pattern, true)) {
-        onPatternMatched.invoke()
-    } else {
-        throw SnapError(message = "Callback url is incorrect")
+        onPatternContained.invoke()
     }
 }
 
@@ -105,6 +126,7 @@ private class SnapWebViewClient(
     }
 
     companion object {
+        const val CALLBACK_KLIK_BCA = "/inquiry" //TODO check whether klik bca use webview/not
         const val CALLBACK_BCA_KLIK_PAY = "?id="
         const val CALLBACK_CIMB_CLICKS = "cimb-clicks/response"
         const val CALLBACK_DANAMON_ONLINE = "/callback?signature="
