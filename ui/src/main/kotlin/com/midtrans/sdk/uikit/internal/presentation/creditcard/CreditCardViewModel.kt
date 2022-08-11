249aff3c5d694b33513ee70e6fda673c88fdbf3d
Muhammad Fauzi Masykur <muhammadfauzimasykur@gmail.com>
2022-08-11 11:24:52 +0700

    update ccViewModel

--- ui/src/main/kotlin/com/midtrans/sdk/uikit/internal/presentation/creditcard/CreditCardViewModel.kt
+++ ui/src/main/kotlin/com/midtrans/sdk/uikit/internal/presentation/creditcard/CreditCardViewModel.kt
@@ -7,41 +7,31 @@ import com.midtrans.sdk.corekit.SnapCore
 import com.midtrans.sdk.corekit.api.callback.Callback
 import com.midtrans.sdk.corekit.api.exception.SnapError
 import com.midtrans.sdk.corekit.api.model.BinResponse
+import com.midtrans.sdk.corekit.api.model.CardTokenResponse
+import com.midtrans.sdk.corekit.api.model.TransactionResponse
+import com.midtrans.sdk.corekit.api.requestbuilder.cardtoken.CreditCardTokenRequestBuilder
+import com.midtrans.sdk.corekit.api.requestbuilder.payment.CreditCardPaymentRequestBuilder
+import com.midtrans.sdk.uikit.R
 import javax.inject.Inject
-
-//class CreditCardViewModel(
-//    val exbinRepository: ExbinRepository
-//): ViewModel() {
-//    val bankIconLiveData = MutableLiveData<String>()
-//
-//    @SuppressLint("CheckResult")
-//    fun updateBankIcon(binNumber: String){
-//        exbinRepository.getBankName(binNumber)
-//            .subscribeOn(Schedulers.io())
-//            .observeOn(AndroidSchedulers.mainThread())
-//            .subscribe({
-//                bankIconLiveData.value = it
-//            },
-//                {}
-//            )
-//    }
-//}
 
 class CreditCardViewModel @Inject constructor(
     private val snapCore: SnapCore
 ) : ViewModel()  {
-    val bankNameLiveData = MutableLiveData<String>()
+
+    val bankIconId = MutableLiveData<Int>()
 
     @SuppressLint("CheckResult")
     fun getBankName(binNumber: String,
-                    clientKet: String) {
+                    clientKey: String) {
         snapCore.getBinData(
             binNumber = binNumber,
-            clientKey = "VT-client-yrHf-c8Sxr-ck8tx",
+            clientKey = clientKey,
             callback = object : Callback<BinResponse> {
                 override fun onSuccess(result: BinResponse) {
                     result.run {
-                        data?.bank.let { bankNameLiveData.value = it }
+                        data?.bankCode?.let {
+                            bankIconId.value = getBankIcon(it?.lowercase())
+                        }
                     }
                 }
 
@@ -50,5 +40,44 @@ class CreditCardViewModel @Inject constructor(
                 }
             }
         )
+    }
+
+    fun getCardToken(
+        cardTokenRequestBuilder: CreditCardTokenRequestBuilder,
+        callback: Callback<CardTokenResponse>
+    ){
+        snapCore.getCardToken(
+            cardTokenRequestBuilder = cardTokenRequestBuilder,
+            callback = callback
+        )
+    }
+
+    fun chargeUsingCard(
+        paymentRequestBuilder: CreditCardPaymentRequestBuilder,
+        snapToken: String,
+        callback: Callback<TransactionResponse>
+    ){
+        snapCore.pay(
+            paymentRequestBuilder = paymentRequestBuilder,
+            snapToken = snapToken,
+            callback = callback
+        )
+    }
+
+    fun setBankIconIdToNull(){
+        bankIconId.value = null
+    }
+
+    private fun getBankIcon(bank: String): Int? {
+
+        return when (bank.lowercase()) {
+            "bri" -> R.drawable.ic_outline_bri_24
+            "bni" -> R.drawable.ic_bank_bni_24
+            "mandiri" -> R.drawable.ic_bank_mandiri_24
+            "bca" -> R.drawable.ic_bank_bca_24
+            "cimb" -> R.drawable.ic_bank_cimb_24
+            "mega" -> R.drawable.ic_bank_mega_24
+            else -> null
+        }
     }
 }
