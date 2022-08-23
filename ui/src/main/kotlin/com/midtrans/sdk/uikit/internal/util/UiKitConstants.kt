8a60c7c7152044ef85a4b0e736cb4a29ca781aa0
jordy.jordy <jordy.jordy@gojek.com>
2022-08-23 19:12:29 +0700

    added UiKitConstants variables

---
+++ ui/src/main/kotlin/com/midtrans/sdk/uikit/internal/util/UiKitConstants.kt
@@ -0,0 +1,9 @@
+package com.midtrans.sdk.uikit.internal.util
+
+object UiKitConstants {
+    //Intent Key
+    const val KEY_TRANSACTION_RESPONSE = "UiKitConstants.key_transaction_response"
+
+    //Transaction Status
+    const val STATUS_PENDING = "pending"
+}
