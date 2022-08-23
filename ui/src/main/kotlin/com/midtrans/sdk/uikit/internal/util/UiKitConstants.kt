c89eba00837ea4c336abd9606221ada7239f6e87
jordy.jordy <jordy.jordy@gojek.com>
2022-08-23 20:56:38 +0700

    rename variable in uikit constants

--- ui/src/main/kotlin/com/midtrans/sdk/uikit/internal/util/UiKitConstants.kt
+++ ui/src/main/kotlin/com/midtrans/sdk/uikit/internal/util/UiKitConstants.kt
@@ -2,7 +2,7 @@ package com.midtrans.sdk.uikit.internal.util
 
 object UiKitConstants {
     //Intent Key
-    const val KEY_TRANSACTION_RESPONSE = "UiKitConstants.key_transaction_response"
+    const val KEY_TRANSACTION_RESULT = "UiKitConstants.key_transaction_result"
 
     //Transaction Status
     const val STATUS_PENDING = "pending"
