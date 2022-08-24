e2e67b06edc79d8777c2c5c8b677a663af01989c
jordy.jordy <jordy.jordy@gojek.com>
2022-08-24 22:08:12 +0700

    add nullable to saveCard variable

--- corekit/src/main/kotlin/com/midtrans/sdk/corekit/internal/network/model/request/PaymentParam.kt
+++ corekit/src/main/kotlin/com/midtrans/sdk/corekit/internal/network/model/request/PaymentParam.kt
@@ -3,7 +3,7 @@ package com.midtrans.sdk.corekit.internal.network.model.request
 internal data class PaymentParam(
     val userId: String? = null,
     val cardToken: String? = null,
-    val saveCard: Boolean = false,
+    val saveCard: Boolean? = false,
     var acquirer: List<String>? = null,
     val point: Double? = null,
     val bank: String? = null,
