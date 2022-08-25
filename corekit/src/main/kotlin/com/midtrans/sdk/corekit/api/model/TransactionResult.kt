3d72865fa4f9099a60297bedf2c34b4e92ef90f8
jordy.jordy <jordy.jordy@gojek.com>
2022-08-25 19:13:15 +0700

    update todo

--- corekit/src/main/kotlin/com/midtrans/sdk/corekit/api/model/TransactionResult.kt
+++ corekit/src/main/kotlin/com/midtrans/sdk/corekit/api/model/TransactionResult.kt
@@ -4,7 +4,7 @@ import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
-data class TransactionResult( //TODO temporary for direct debit only
+data class TransactionResult( //TODO temporary for direct debit only, revisit when updating callback for host app
     val status: String,
     val transactionId: String,
     val paymentType: String
