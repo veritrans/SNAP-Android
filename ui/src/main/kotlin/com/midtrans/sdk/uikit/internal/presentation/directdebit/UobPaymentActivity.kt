7703fde92e3c200fc31e3ab28edcb69469cfe287
jordy.jordy <jordy.jordy@gojek.com>
2022-09-13 21:27:32 +0700

    add uob payment activity to dagger component

--- ui/src/main/kotlin/com/midtrans/sdk/uikit/internal/presentation/directdebit/UobPaymentActivity.kt
+++ ui/src/main/kotlin/com/midtrans/sdk/uikit/internal/presentation/directdebit/UobPaymentActivity.kt
@@ -2,10 +2,46 @@ package com.midtrans.sdk.uikit.internal.presentation.directdebit
 
 import android.content.Context
 import android.content.Intent
+import android.os.Bundle
 import com.midtrans.sdk.uikit.internal.base.BaseActivity
+import com.midtrans.sdk.uikit.internal.di.DaggerUiKitComponent
 import com.midtrans.sdk.uikit.internal.model.CustomerInfo
 
 class UobPaymentActivity : BaseActivity() {
+
+    private val snapToken: String by lazy {
+        intent.getStringExtra(EXTRA_SNAP_TOKEN)
+            ?: throw RuntimeException("Snap token must not be empty")
+    }
+
+    private val uobMode: String by lazy {
+        intent.getStringExtra(EXTRA_UOB_MODE)
+            ?: throw RuntimeException("Uob mode must not be empty")
+    }
+
+    private val amount: String by lazy {
+        intent.getStringExtra(EXTRA_AMOUNT)
+            ?: throw RuntimeException("Total amount must not be empty")
+    }
+
+    private val orderId: String by lazy {
+        intent.getStringExtra(EXTRA_ORDER_ID)
+            ?: throw RuntimeException("Order ID must not be empty")
+    }
+
+    private val customerInfo: CustomerInfo? by lazy {
+        intent.getParcelableExtra(EXTRA_CUSTOMER_INFO) as? CustomerInfo
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        DaggerUiKitComponent.builder()
+            .applicationContext(this.applicationContext)
+            .build()
+            .inject(this)
+    }
+
     companion object {
         private const val EXTRA_SNAP_TOKEN = "directDebit.uobPayment.extra.snap_token"
         private const val EXTRA_UOB_MODE = "directDebit.uobPayment.extra.uob_mode"
