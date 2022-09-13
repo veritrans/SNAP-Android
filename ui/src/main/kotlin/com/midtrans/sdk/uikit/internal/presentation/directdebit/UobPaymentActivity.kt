d8bf0d00dccb65ca0a61f2b078f053197d558186
jordy.jordy <jordy.jordy@gojek.com>
2022-09-13 21:45:15 +0700

    add content for uob payment

--- ui/src/main/kotlin/com/midtrans/sdk/uikit/internal/presentation/directdebit/UobPaymentActivity.kt
+++ ui/src/main/kotlin/com/midtrans/sdk/uikit/internal/presentation/directdebit/UobPaymentActivity.kt
@@ -3,14 +3,26 @@ package com.midtrans.sdk.uikit.internal.presentation.directdebit
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringArrayResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import com.midtrans.sdk.corekit.api.model.PaymentType
 import com.midtrans.sdk.corekit.api.model.TransactionResponse
 import com.midtrans.sdk.uikit.R
 import com.midtrans.sdk.uikit.internal.base.BaseActivity
 import com.midtrans.sdk.uikit.internal.di.DaggerUiKitComponent
 import com.midtrans.sdk.uikit.internal.model.CustomerInfo
+import com.midtrans.sdk.uikit.internal.view.*
 
 class UobPaymentActivity : BaseActivity() {
 
@@ -48,6 +60,115 @@ class UobPaymentActivity : BaseActivity() {
     }
 
     @Composable
+    private fun UobPaymentContent(
+        uobMode: String,
+        amount: String,
+        orderId: String,
+        customerInfo: CustomerInfo?,
+        response: TransactionResponse?
+    ) {
+        var isCustomerDetailExpanded by remember { mutableStateOf(false) }
+        var isInstructionExpanded by remember { mutableStateOf(false) }
+        val title = stringResource(getTitleId(uobMode = uobMode))
+        val url = getUobDeeplinkUrl(uobMode, response)
+
+        if (url.isEmpty()) {
+            Column(
+                modifier = Modifier
+                    .background(SnapColors.getARGBColor(SnapColors.OVERLAY_WHITE))
+                    .fillMaxHeight(1f)
+            ) {
+                SnapAppBar(
+                    title = title,
+                    iconResId = R.drawable.ic_arrow_left
+                ) {
+                    onBackPressed()
+                }
+                SnapOverlayExpandingBox(
+                    modifier = Modifier
+                        .weight(1f)
+                        .padding(all = 16.dp),
+                    isExpanded = isCustomerDetailExpanded,
+                    mainContent = {
+                        SnapTotal(
+                            amount = amount,
+                            orderId = orderId,
+                            canExpand = customerInfo != null,
+                            remainingTime = null
+                        ) {
+                            isCustomerDetailExpanded = it
+                        }
+                    },
+                    expandingContent = customerInfo?.let {
+                        {
+                            SnapCustomerDetail(
+                                name = customerInfo.name,
+                                phone = customerInfo.phone,
+                                addressLines = customerInfo.addressLines
+                            )
+                        }
+                    },
+                    followingContent = {
+                        Column(
+                            modifier = Modifier
+                                .verticalScroll(state = rememberScrollState())
+                                .padding(top = 16.dp)
+                                .fillMaxWidth()
+
+                        ) {
+                            SnapText(stringResource(getInstructionId(uobMode)))
+                            SnapInstructionButton(
+                                modifier = Modifier.padding(top = 28.dp),
+                                isExpanded = isInstructionExpanded,
+                                iconResId = R.drawable.ic_help,
+                                title = stringResource(R.string.bca_klik_pay_how_to_pay_title),
+                                onExpandClick = { isInstructionExpanded = !isInstructionExpanded },
+                                expandingContent = {
+                                    Column {
+                                        AnimatedVisibility(visible = isInstructionExpanded) {
+                                            SnapNumberedList(
+                                                list = stringArrayResource(getHowToPayId(uobMode)).toList()
+                                            )
+                                        }
+                                    }
+                                }
+                            )
+                        }
+                    }
+                )
+                SnapButton(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 40.dp, bottom = 16.dp, start = 16.dp, end = 16.dp),
+                    enabled = true,
+                    text = stringResource(id = getUobCta(uobMode)),
+                    style = SnapButton.Style.PRIMARY
+                ) {
+                    //TODO viewmodel here
+//                    viewModel.payDirectDebit(
+//                        snapToken = snapToken,
+//                        paymentType = paymentType,
+//                        userId = userId
+//                    )
+                }
+            }
+        } else {
+            //todo open deeplink
+        }
+    }
+
+    private fun getUobDeeplinkUrl(
+        uobMode: String,
+        response: TransactionResponse?
+    ): String {
+        return when (uobMode) {
+            PaymentType.UOB_EZPAY_WEB -> response?.uobEzpayWebUrl.orEmpty()
+            PaymentType.UOB_EZPAY_APP -> response?.uobEzpayDeeplinkUrl.orEmpty()
+            else -> ""
+        }
+    }
+
+    @Composable
     private fun getTitleId(uobMode: String): Int {
         return when (uobMode) {
             PaymentType.UOB_EZPAY_APP -> R.string.uob_tmrw_method_name
@@ -70,6 +191,15 @@ class UobPaymentActivity : BaseActivity() {
         return when (uobMode) {
             PaymentType.UOB_EZPAY_WEB -> R.array.uob_web_how_to_pay
             PaymentType.UOB_EZPAY_APP -> R.array.uob_tmrw_how_to_pay
+            else -> 0
+        }
+    }
+
+    @Composable
+    private fun getUobCta(uobMode: String): Int {
+        return when (uobMode) {
+            PaymentType.UOB_EZPAY_WEB -> R.string.uob_web_cta
+            PaymentType.UOB_EZPAY_APP -> R.string.uob_tmrw_cta
             else -> 0
         }
     }
