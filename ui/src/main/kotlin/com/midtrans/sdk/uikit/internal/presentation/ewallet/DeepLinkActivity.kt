0958b12908d4e0c328e5987e4eba8d4c04dcefa0
jordy.jordy <jordy.jordy@gojek.com>
2022-09-20 23:10:58 +0700

    revert changes for deeplink activity

--- ui/src/main/kotlin/com/midtrans/sdk/uikit/internal/presentation/ewallet/DeepLinkActivity.kt
+++ ui/src/main/kotlin/com/midtrans/sdk/uikit/internal/presentation/ewallet/DeepLinkActivity.kt
@@ -13,6 +13,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -26,20 +27,9 @@ import com.midtrans.sdk.uikit.internal.view.SnapWebView
 
 class DeepLinkActivity : BaseActivity() {
 
-    private val url: String by lazy {
-        intent.getStringExtra(EXTRA_URL) ?: throw RuntimeException("Url must not be empty")
-    }
-
-    private val paymentType: String by lazy {
-        intent.getStringExtra(EXTRA_PAYMENT_TYPE)
-            ?: throw RuntimeException("Payment type must not be empty")
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            Content(paymentType = paymentType, url = url)
-        }
+        setContent { Content(paymentType = paymentType, url = url) }
     }
 
     @Composable
@@ -57,10 +47,22 @@ class DeepLinkActivity : BaseActivity() {
                 url = url,
                 onPageStarted = { loading = true },
                 onPageFinished = { loading = false },
-                urlLoadingOverride = { _, url ->
-                    Log.d("urlOverload", url)
+                urlLoadingOverride = { webview, url ->
+                    Log.e("urlOverload", url)
 
-                    openDeeplink(paymentType, url)
+                    if (url.contains("gojek") || url.contains("shopee")) {  // TODO: fill with exact scheme
+                        try {
+                            intent = Intent(Intent.ACTION_VIEW)
+                            intent.setData(Uri.parse(url))
+                            startActivity(intent)
+                            true
+                        } catch (e: Throwable) {
+                            openAppInPlayStore()
+                            true
+                        }
+                    } else {
+                        false
+                    }
                 }
             )
             if (loading) {
@@ -96,38 +98,32 @@ class DeepLinkActivity : BaseActivity() {
 
     @Preview
     @Composable
-    private fun ForPreview() {
+    private fun forPreview() {
         Content(paymentType = PaymentType.GOPAY, url = "http://")
     }
 
-    private fun openDeeplink(
-        paymentType: String,
-        url: String
-    ): Boolean {
+    private fun getAppPackageName(): String {
         return when (paymentType) {
-            PaymentType.GOPAY, PaymentType.SHOPEEPAY, PaymentType.UOB_EZPAY_APP -> {
-                try {
-                    intent = Intent(Intent.ACTION_VIEW)
-                    intent.data = Uri.parse(url)
-                    startActivity(intent)
-                    finish()
-                    true
-                } catch (e: Throwable) {
-                    openAppInPlayStore() //TODO check with pak wahyu should this be removed?
-                    false
-                }
-            }
-            else -> {
-                false
-            }
+            PaymentType.GOPAY -> GOJEK_PACKAGE_NAME
+            PaymentType.SHOPEEPAY -> SHOPEE_PACKAGE_NAME
+            PaymentType.SHOPEEPAY_QRIS -> SHOPEE_PACKAGE_NAME
+            else -> GOJEK_PACKAGE_NAME
         }
+    }
+
+    private val url: String by lazy {
+        intent.getStringExtra(EXTRA_URL) ?: throw RuntimeException("Url must not be empty")
+    }
+
+    private val paymentType: String by lazy {
+        intent.getStringExtra(EXTRA_PAYMENT_TYPE)
+            ?: throw RuntimeException("Payment type must not be empty")
     }
 
     private val redirectionTitle by lazy {
         mapOf(
             Pair(PaymentType.GOPAY, R.string.redirection_screen_gopay_main_message),
             Pair(PaymentType.SHOPEEPAY, R.string.redirection_screen_shopeepay_main_message),
-            Pair(PaymentType.UOB_EZPAY_APP, R.string.redirection_to_uob_tmrw_message) //TODO get correct copy for uob app
         )
     }
 
@@ -135,7 +131,6 @@ class DeepLinkActivity : BaseActivity() {
         mapOf(
             Pair(PaymentType.GOPAY, R.string.redirection_screen_gopay_cta),
             Pair(PaymentType.SHOPEEPAY, R.string.redirection_screen_shopeepay_cta),
-            Pair(PaymentType.UOB_EZPAY_APP, R.string.redirection_to_uob_tmrw_message) //TODO get correct copy for uob app
         )
     }
 
@@ -157,27 +152,18 @@ class DeepLinkActivity : BaseActivity() {
         }
     }
 
-    private fun getAppPackageName(): String {
-        return when (paymentType) {
-            PaymentType.GOPAY -> GOJEK_PACKAGE_NAME
-            PaymentType.SHOPEEPAY -> SHOPEE_PACKAGE_NAME
-            PaymentType.SHOPEEPAY_QRIS -> SHOPEE_PACKAGE_NAME
-            PaymentType.UOB_EZPAY_APP -> UOB_TMRW_PACKAGE_NAME
-            else -> GOJEK_PACKAGE_NAME
-        }
-    }
-
     companion object {
         private const val EXTRA_URL = "deeplinkactivity.extra.url"
         private const val EXTRA_PAYMENT_TYPE = "deeplinkactivity.extra.payment_type"
         private const val GOJEK_PACKAGE_NAME = "com.gojek.app"
         private const val SHOPEE_PACKAGE_NAME = "com.shopee.id"
-        private const val UOB_TMRW_PACKAGE_NAME = "com.uob.id.digitalbank"
+
 
         fun getIntent(
             activityContext: Context,
             paymentType: String,
             url: String
+
         ): Intent {
             return Intent(activityContext, DeepLinkActivity::class.java).apply {
                 putExtra(EXTRA_URL, url)
