b7175e44bd1233e66e296165075989bcb2080976
jordy.jordy <jordy.jordy@gojek.com>
2022-10-06 19:44:32 +0700

    update sample ui after added onSelectedOption in dropdown menu

--- app/src/main/kotlin/com/midtrans/sdk/sample/SampleUiActivity.kt
+++ app/src/main/kotlin/com/midtrans/sdk/sample/SampleUiActivity.kt
@@ -191,7 +191,7 @@ class SampleUiActivity : AppCompatActivity() {
 //                }
 //            )
 
-            InstallmentDropdownMenu(title = "Option", optionList = listOf("option_1", "option_2", "option_3"))
+            InstallmentDropdownMenu(title = "Option", optionList = listOf("option_1", "option_2", "option_3"), onOptionsSelected = {} )
 
             SnapNumberedListItem(
                 number = "1.",
