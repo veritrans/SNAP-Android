65f195299abd27c3e97226c1ea08a1befbfd8491
jordy.jordy <jordy.jordy@gojek.com>
2022-10-18 21:41:47 +0700

    replace dependency clevertap with mixpanel

--- corekit/build.gradle.kts
+++ corekit/build.gradle.kts
@@ -85,7 +85,7 @@ dependencies {
     implementation(com.gtf.snap.CommonLibraries.androidxDatastore)
     implementation(com.gtf.snap.JodaTimeLibraries.jodaTime)
     implementation(com.gtf.snap.CommonLibraries.uuid)
-    implementation(com.gtf.snap.CommonLibraries.clevertap)
+    implementation(com.gtf.snap.CommonLibraries.mixpanel)
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.3")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
