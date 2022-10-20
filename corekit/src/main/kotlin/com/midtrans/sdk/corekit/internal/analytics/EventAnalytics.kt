255819a777627859da1dcda955bedf7e4c4dbc33
jordy.jordy <jordy.jordy@gojek.com>
2022-10-20 16:43:55 +0700

    add event analytics class

---
+++ corekit/src/main/kotlin/com/midtrans/sdk/corekit/internal/analytics/EventAnalytics.kt
@@ -0,0 +1,41 @@
+package com.midtrans.sdk.corekit.internal.analytics
+
+internal class EventAnalytics(
+    private val mixpanelTracker: MixpanelTracker
+) {
+    fun setUserIdentity(
+        id: String,
+        name: String,
+        extras: Map<String, String>
+    ) {
+        mixpanelTracker.setUserIdentity(
+            id = id,
+            name = name,
+            extras = extras
+        )
+    }
+
+    fun testTracker() {
+        mixpanelTracker.trackEvent("testEvent")
+    }
+
+    //TODO will be implemented separately
+    fun trackSnapPageViewed() {}
+    fun trackSnapOrderDetailsViewed() {}
+    fun trackSnapChargeRequest() {}
+    fun trackSnapCtaClicked() {}
+    fun trackSnapChargeResult() {}
+    fun trackSnapHowToPayViewed() {}
+    fun trackSnapAccountNumberCopied() {}
+    fun trackSnapPaymentNumberButtonRetried() {}
+    fun trackSnapExbinResponse() {}
+    fun trackSnapPageClosed() {}
+    fun trackSnapOpenDeeplink() {}
+    fun trackSnapError() {}
+    fun trackSnap3dsResult() {}
+    fun trackSnapTokenizationResult() {}
+    fun trackSnapCtaError() {}
+    fun trackSnapGetTokenRequest() {}
+    fun trackSnapGetTokenResult() {}
+    fun trackSnapCustomerDataInput() {}
+}
