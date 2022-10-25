82cac79ddeaac56ff5aa3d1274d3f0253f7bee02
jordy.jordy <jordy.jordy@gojek.com>
2022-10-25 21:18:41 +0700

    add method to register super properties

--- corekit/src/main/kotlin/com/midtrans/sdk/corekit/internal/analytics/EventAnalytics.kt
+++ corekit/src/main/kotlin/com/midtrans/sdk/corekit/internal/analytics/EventAnalytics.kt
@@ -1,5 +1,13 @@
 package com.midtrans.sdk.corekit.internal.analytics
 
+import com.midtrans.sdk.corekit.internal.analytics.EventName.PROPERTY_MERCHANT_ID
+import com.midtrans.sdk.corekit.internal.analytics.EventName.PROPERTY_MERCHANT_NAME
+import com.midtrans.sdk.corekit.internal.analytics.EventName.PROPERTY_PLATFORM
+import com.midtrans.sdk.corekit.internal.analytics.EventName.PROPERTY_SDK_TYPE
+import com.midtrans.sdk.corekit.internal.analytics.EventName.PROPERTY_SDK_VERSION
+import com.midtrans.sdk.corekit.internal.analytics.EventName.PROPERTY_SERVICE_TYPE
+import com.midtrans.sdk.corekit.internal.analytics.EventName.PROPERTY_SOURCE_TYPE
+
 class EventAnalytics(
     private val mixpanelTracker: MixpanelTracker
 ) {
@@ -13,14 +21,18 @@ class EventAnalytics(
             name = name,
             extras = extras
         )
+        registerSuperProperties(saudagarId = id, merchantName = name)
     }
 
     fun testTracker() {
         mixpanelTracker.trackEvent("testEvent")
     }
 
-    //TODO will be implemented separately
     fun trackSnapPageViewed() {}
+
+
+    //TODO will be implemented separately
+    fun trackSnapCustomerDataInput() {}
     fun trackSnapOrderDetailsViewed() {}
     fun trackSnapChargeRequest() {}
     fun trackSnapCtaClicked() {}
@@ -37,5 +49,27 @@ class EventAnalytics(
     fun trackSnapCtaError() {}
     fun trackSnapGetTokenRequest() {}
     fun trackSnapGetTokenResult() {}
-    fun trackSnapCustomerDataInput() {}
+
+    private fun registerSuperProperties(
+        saudagarId: String,
+        merchantName: String
+    ) {
+        mixpanelTracker.registerSuperProperties(
+            mapOf(
+                PROPERTY_PLATFORM to "Mobile",
+                PROPERTY_SDK_VERSION to "1.1.1", //TODO get sdk version
+                PROPERTY_SDK_TYPE to "UI",
+                PROPERTY_MERCHANT_ID to saudagarId,
+                PROPERTY_MERCHANT_NAME to merchantName,
+                PROPERTY_SOURCE_TYPE to "midtrans-mobile",
+                PROPERTY_SERVICE_TYPE to "snap"
+            )
+        )
+    }
+
+    fun registerCommonTransactionProperties(
+
+    ) {
+        
+    }
 }
