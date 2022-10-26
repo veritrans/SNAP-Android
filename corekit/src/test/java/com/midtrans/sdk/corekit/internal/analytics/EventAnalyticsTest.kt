73e21de5ae99f2297df8e463ff2ce42955c3f619
jordy.jordy <jordy.jordy@gojek.com>
2022-10-26 17:33:43 +0700

    add event analytics test

---
+++ corekit/src/test/java/com/midtrans/sdk/corekit/internal/analytics/EventAnalyticsTest.kt
@@ -0,0 +1,52 @@
+package com.midtrans.sdk.corekit.internal.analytics
+
+import com.midtrans.sdk.corekit.internal.analytics.EventName.EVENT_SNAP_GET_TOKEN_REQUEST
+import com.midtrans.sdk.corekit.internal.analytics.EventName.EVENT_SNAP_GET_TOKEN_RESULT
+import com.midtrans.sdk.corekit.internal.analytics.EventName.PROPERTY_RESPONSE_TIME
+import com.midtrans.sdk.corekit.internal.analytics.EventName.PROPERTY_SNAP_TOKEN
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import org.mockito.Mock
+import org.mockito.MockitoAnnotations
+import org.mockito.kotlin.verify
+
+internal class EventAnalyticsTest {
+    private lateinit var closeable: AutoCloseable
+    private lateinit var eventAnalytics: EventAnalytics
+
+    @Mock
+    private lateinit var mixpanelTracker: MixpanelTracker
+
+    @Before
+    fun setUp() {
+        closeable = MockitoAnnotations.openMocks(this)
+        eventAnalytics = EventAnalytics(mixpanelTracker)
+    }
+
+    @After
+    fun tearDown() {
+        closeable.close()
+    }
+
+    @Test
+    fun verifyTrackSnapGetTokenRequest() {
+        eventAnalytics.trackSnapGetTokenRequest("token")
+        verify(mixpanelTracker).trackEvent(
+            eventName = EVENT_SNAP_GET_TOKEN_REQUEST,
+            properties = mapOf(PROPERTY_SNAP_TOKEN to "token")
+        )
+    }
+
+    @Test
+    fun verifyTrackSnapGetTokenResult() {
+        eventAnalytics.trackSnapGetTokenResult("token")
+        verify(mixpanelTracker).trackEvent(
+            eventName = EVENT_SNAP_GET_TOKEN_RESULT,
+            properties = mapOf(
+                PROPERTY_SNAP_TOKEN to "token",
+                PROPERTY_RESPONSE_TIME to ""
+            )
+        )
+    }
+}
