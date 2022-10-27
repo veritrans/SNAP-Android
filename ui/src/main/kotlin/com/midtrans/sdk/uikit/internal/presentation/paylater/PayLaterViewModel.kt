776d0a87319aa77a1e8d9b566f6f6ce680cf3c71
jordy.jordy <jordy.jordy@gojek.com>
2022-10-27 20:12:57 +0700

    implement tracker charge request and result for pay later

--- ui/src/main/kotlin/com/midtrans/sdk/uikit/internal/presentation/paylater/PayLaterViewModel.kt
+++ ui/src/main/kotlin/com/midtrans/sdk/uikit/internal/presentation/paylater/PayLaterViewModel.kt
@@ -8,13 +8,15 @@ import com.midtrans.sdk.corekit.api.callback.Callback
 import com.midtrans.sdk.corekit.api.exception.SnapError
 import com.midtrans.sdk.corekit.api.model.TransactionResponse
 import com.midtrans.sdk.corekit.api.requestbuilder.payment.PayLaterPaymentRequestBuilder
+import com.midtrans.sdk.corekit.internal.analytics.PageName
+import com.midtrans.sdk.uikit.internal.base.BaseViewModel
 import com.midtrans.sdk.uikit.internal.util.DateTimeUtil
 import javax.inject.Inject
 
 internal class PayLaterViewModel @Inject constructor(
     private val snapCore: SnapCore,
     private val dateTimeUtil: DateTimeUtil
-): ViewModel() {
+): BaseViewModel(snapCore) {
     private val _transactionResponseLiveData = MutableLiveData<TransactionResponse>()
     private var expiredTime = dateTimeUtil.plusDateBy(dateTimeUtil.getCurrentMillis(), 1) //TODO later get value from request snap if set
 
@@ -27,11 +29,16 @@ internal class PayLaterViewModel @Inject constructor(
         val builder = PayLaterPaymentRequestBuilder()
             .withPaymentType(paymentType)
 
+        trackSnapChargeRequest(
+            pageName = PageName.AKULAKU_PAGE,
+            paymentMethodName = paymentType
+        )
         snapCore.pay(
             snapToken = snapToken,
             paymentRequestBuilder = builder,
             callback = object : Callback<TransactionResponse> {
                 override fun onSuccess(result: TransactionResponse) {
+                    trackSnapChargeResult(result)
                     _transactionResponseLiveData.value = result
                 }
 
