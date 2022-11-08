3d754322c1611afeb00c45f8c108d23f4ed392b0
jordy.jordy <jordy.jordy@gojek.com>
2022-11-08 19:46:07 +0700

    add data class ItemInfo

---
+++ ui/src/main/kotlin/com/midtrans/sdk/uikit/internal/model/ItemInfo.kt
@@ -0,0 +1,11 @@
+package com.midtrans.sdk.uikit.internal.model
+
+import android.os.Parcelable
+import com.midtrans.sdk.corekit.api.model.ItemDetails
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class ItemInfo(
+    val itemDetails: List<ItemDetails>,
+    val totalAmount: Long
+): Parcelable
