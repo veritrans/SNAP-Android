bf5039c4f10e1d0e91fad7b52ef163f3adda8fe7
Ferdian Julianto <Juliantoferdian24@gmail.com>
2022-11-09 18:02:12 +0700

    feat: Send Data From Demo to ProductListActivity.kt

--- app/src/main/kotlin/com/midtrans/sdk/sample/presentation/config/DemoConfigurationActivity.kt
+++ app/src/main/kotlin/com/midtrans/sdk/sample/presentation/config/DemoConfigurationActivity.kt
@@ -1,17 +1,164 @@
 package com.midtrans.sdk.sample.presentation.config
 
 import android.os.Bundle
+import android.os.Parcelable
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.unit.dp
+import com.midtrans.sdk.sample.presentation.shop.ProductListActivity
+import com.midtrans.sdk.uikit.api.model.CustomColors
+import com.midtrans.sdk.uikit.external.UiKitApi
+import com.midtrans.sdk.uikit.internal.util.AssetFontLoader
+import com.midtrans.sdk.uikit.internal.view.SnapTextField
+import com.midtrans.sdk.uikit.internal.view.SnapTypography
+import kotlinx.parcelize.Parcelize
+
 
 class DemoConfigurationActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        
-        setContent{
-            Text(text = "Test")
+        buildUiKit()
+        setContent {
+            MainContent()
         }
     }
+
+    @Composable
+    fun MainContent() {
+        val state = remember {
+            InputState(
+                color = "Blue",
+                installment = "No Installment"
+            )
+        }
+        Column(Modifier.padding(16.dp)) {
+            DropdownMenu(
+                title = "Installment",
+                optionList = listOf(
+                    "No Installment",
+                    "Mandiri",
+                    "BCA",
+                    "BNI",
+                    "BRI",
+                    "CIMB",
+                    "MayBank",
+                    "Offline"
+                ),
+                state = state
+            )
+            DropdownMenu(
+                title = "Color Theme",
+                optionList = listOf("Blue", "Red", "Green"),
+                state = state
+            )
+            Text(text = state.installment)
+            Text(text = state.color)
+
+            Button(onClick = {
+                startActivity(
+                    ProductListActivity.getProductListActivity(
+                        this@DemoConfigurationActivity,
+                        state.color,
+                        state.installment
+                    )
+                )
+            }) {
+                Text(text = "Simple Button")
+            }
+        }
+    }
+
+    @OptIn(ExperimentalMaterialApi::class)
+    @Composable
+    fun DropdownMenu(title: String, optionList: List<String>, state: InputState) {
+        val options by remember { mutableStateOf(optionList) }
+        var expanded by remember { mutableStateOf(false) }
+        var selectedOptionText by remember { mutableStateOf(options[0]) }
+
+        Column(modifier = Modifier.fillMaxWidth(1f), horizontalAlignment = Alignment.Start) {
+            Text(
+                modifier = Modifier.padding(top = 16.dp, bottom = 20.dp),
+                text = title,
+                style = SnapTypography.STYLES.snapTextMediumMedium
+            )
+            ExposedDropdownMenuBox(
+                modifier = Modifier.padding(bottom = 10.dp),
+                expanded = expanded,
+                onExpandedChange = { expanded = !expanded }
+            ) {
+                SnapTextField(
+                    modifier = Modifier.fillMaxWidth(1f),
+                    readOnly = true,
+                    value = TextFieldValue(selectedOptionText),
+                    onValueChange = {},
+                    isFocused = false,
+                    enabled = true,
+                    onFocusChange = {},
+                    trailingIcon = {
+                        ExposedDropdownMenuDefaults.TrailingIcon(
+                            expanded = expanded
+                        )
+                    },
+                    textStyle = SnapTypography.STYLES.snapTextMediumRegular
+                )
+                ExposedDropdownMenu(
+                    modifier = Modifier.fillMaxWidth(1f),
+                    expanded = expanded,
+                    onDismissRequest = { expanded = false }
+                ) {
+                    options.forEach { selectionOption ->
+                        DropdownMenuItem(
+                            onClick = {
+                                selectedOptionText = selectionOption
+                                expanded = false
+                                when (title) {
+                                    "Installment" -> state.installment = selectedOptionText
+                                    "Color Theme" -> state.color = selectedOptionText
+                                }
+                            },
+                            enabled = true
+                        ) {
+                            Text(
+                                text = selectionOption,
+                                style = SnapTypography.STYLES.snapTextMediumRegular
+                            )
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private fun buildUiKit() {
+        UiKitApi.Builder()
+            .withContext(this.applicationContext)
+            .withMerchantUrl("https://fiesta-point-sample.herokuapp.com/")
+            .withMerchantClientKey("SB-Mid-client-hOWJXiCCDRvT0RGr")
+            .withFontFamily(AssetFontLoader.fontFamily("fonts/SourceSansPro-Regular.ttf", this))
+            .withCustomColors(
+                CustomColors(
+                    interactiveFillInverse = 0x0e4e95,
+                    textInverse = 0xFFFFFF,
+                    supportNeutralFill = 0x3e71aa
+                )
+            )
+            .build()
+    }
+}
+
+class InputState(
+    color: String,
+    installment: String
+) {
+    var installment by mutableStateOf(installment)
+    var color by mutableStateOf(color)
 }
--- app/src/main/kotlin/com/midtrans/sdk/sample/presentation/shop/ProductListActivity.kt
+++ app/src/main/kotlin/com/midtrans/sdk/sample/presentation/shop/ProductListActivity.kt
@@ -1,39 +1,53 @@
 package com.midtrans.sdk.sample.presentation.shop
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Text
 import com.midtrans.sdk.sample.presentation.shop.component.ProductListPage
-import com.midtrans.sdk.uikit.api.model.CustomColors
-import com.midtrans.sdk.uikit.external.UiKitApi
-import com.midtrans.sdk.uikit.internal.util.AssetFontLoader
 
 class ProductListActivity : ComponentActivity() {
 
+    private val inputColor: String by lazy {
+        intent.getStringExtra(EXTRA_INPUT_COLOR)
+            ?: throw RuntimeException("Order ID must not be empty")
+    }
+
+    private val installment: String by lazy {
+        intent.getStringExtra(EXTRA_INPUT_INSTALLMENT)
+            ?: throw RuntimeException("Order ID must not be empty")
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        buildUiKit()
         setContent {
-            ProductListPage {
-                val intent = OrderReviewActivity.getOrderReviewActivityIntent(this, it)
-                startActivity(intent)
+            Column {
+                Text(text = inputColor)
+                Text(text = installment)
+                ProductListPage {
+                    val intent = OrderReviewActivity.getOrderReviewActivityIntent(this@ProductListActivity, it)
+                    startActivity(intent)
+                }
             }
         }
     }
 
-    private fun buildUiKit() {
-        UiKitApi.Builder()
-            .withContext(this.applicationContext)
-            .withMerchantUrl("https://fiesta-point-sample.herokuapp.com/")
-            .withMerchantClientKey("SB-Mid-client-hOWJXiCCDRvT0RGr")
-            .withFontFamily(AssetFontLoader.fontFamily("fonts/SourceSansPro-Regular.ttf", this))
-            .withCustomColors(
-                CustomColors(
-                    interactiveFillInverse = 0x0e4e95,
-                    textInverse = 0xFFFFFF,
-                    supportNeutralFill = 0x3e71aa
-                )
-            )
-            .build()
+    companion object {
+        private const val EXTRA_INPUT_COLOR = "productList.extra.inputColor"
+        private const val EXTRA_INPUT_INSTALLMENT = "productList.extra.installment"
+
+        fun getProductListActivity(
+            activityContext: Context,
+            color: String,
+            installment: String
+        ): Intent {
+            return Intent(activityContext, ProductListActivity::class.java).apply {
+                putExtra(EXTRA_INPUT_COLOR, color)
+                putExtra(EXTRA_INPUT_INSTALLMENT, installment)
+            }
+        }
     }
 }
