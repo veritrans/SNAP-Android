12732a5322fc1ef4cf8e4198987718e94a4f4925
jordy.jordy <jordy.jordy@gojek.com>
2022-11-11 11:42:41 +0700

    revert price to be nullable

--- corekit/src/main/kotlin/com/midtrans/sdk/corekit/api/model/ItemDetails.kt
+++ corekit/src/main/kotlin/com/midtrans/sdk/corekit/api/model/ItemDetails.kt
@@ -6,7 +6,7 @@ import kotlinx.parcelize.Parcelize
 @Parcelize
 open class ItemDetails(
     val id: String? = null,
-    val price: Double = 0.00,
+    val price: Double? = null,
     val quantity: Int = 0,
     val name: String? = null,
 ) : Parcelable
