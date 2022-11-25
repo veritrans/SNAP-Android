c5f38a260d13c8dfa114de1ffb945b0e156138b8
jordy.jordy <jordy.jordy@gojek.com>
2022-11-25 10:56:42 +0700

    add trackSnapNotice in BaseViewModel

--- ui/src/main/kotlin/com/midtrans/sdk/uikit/internal/base/BaseViewModel.kt
+++ ui/src/main/kotlin/com/midtrans/sdk/uikit/internal/base/BaseViewModel.kt
@@ -235,4 +235,18 @@ internal open class BaseViewModel : ViewModel() {
             )
         }
     }
+
+    protected fun trackSnapNotice(
+        pageName: String,
+        paymentMethodName: String,
+        statusText: String,
+        noticeMessage: String?
+    ) {
+        eventAnalytics?.trackSnapNotice(
+            pageName = pageName,
+            paymentMethodName = paymentMethodName,
+            statusText = statusText,
+            noticeMessage = noticeMessage
+        )
+    }
 }
