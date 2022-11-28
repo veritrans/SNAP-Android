4a0f4b80d81de6f854f0141bc9fea0fba4d195ea
jordy.jordy <jordy.jordy@gojek.com>
2022-11-28 18:30:11 +0700

    add headers for snap request interceptor

--- corekit/src/main/kotlin/com/midtrans/sdk/corekit/internal/network/SnapRequestInterceptor.kt
+++ corekit/src/main/kotlin/com/midtrans/sdk/corekit/internal/network/SnapRequestInterceptor.kt
@@ -1,10 +1,19 @@
 package com.midtrans.sdk.corekit.internal.network
 
+import com.midtrans.sdk.corekit.BuildConfig
 import okhttp3.Interceptor
 import okhttp3.Response
 
-internal class SnapRequestInterceptor() : Interceptor {
+internal class SnapRequestInterceptor : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
-        TODO("Not yet implemented")
+        val request = chain.request()
+
+        return chain.proceed(
+            request.newBuilder()
+                .addHeader("X-Source", "mobile-android")
+                .addHeader("X-Source-Version", "android-${BuildConfig.SDK_VERSION}")
+                .addHeader("X-Service", "snap")
+                .build()
+        )
     }
 }
