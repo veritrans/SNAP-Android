a3f1c8a550fbda8a7a68e48468b7d117d94d2ae8
jordy.jordy <jordy.jordy@gojek.com>
2022-11-30 21:48:40 +0700

    add method to get error information

--- corekit/src/main/kotlin/com/midtrans/sdk/corekit/api/exception/SnapError.kt
+++ corekit/src/main/kotlin/com/midtrans/sdk/corekit/api/exception/SnapError.kt
@@ -2,12 +2,14 @@ package com.midtrans.sdk.corekit.api.exception
 
 open class SnapError(cause: Throwable? = null, message: String? = null): Throwable(cause = cause, message = message) {
 
+    fun getErrorInformation(): String {
+        return this.message ?: this.cause?.javaClass?.name ?: this.javaClass.name
+    }
 
     companion object{
         // messages
         const val MESSAGE_ERROR_EMPTY_RESPONSE = "Failed to retrieve response from server"
-        const val MESSAGE_ERROR_EMPTY_MERCHANT_URL =
-            "Merchant base url is empty. Please set merchant base url on SDK"
+        const val MESSAGE_ERROR_EMPTY_MERCHANT_URL = "Merchant base url is empty. Please set merchant base url on SDK"
         const val MESSAGE_ERROR_INVALID_DATA_SUPPLIED = "Invalid data supplied to SDK."
         const val MESSAGE_ERROR_FAILED_TO_CONNECT_TO_SERVER = "Failed to connect to server."
         const val MESSAGE_ERROR_ALREADY_RUNNING = "Failed to connect to server."
