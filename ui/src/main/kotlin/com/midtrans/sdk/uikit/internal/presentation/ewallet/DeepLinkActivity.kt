207e4f63b2f755906a728d3d72e515ea63a34bdc
Ferdian Julianto <Juliantoferdian24@gmail.com>
2023-01-24 13:54:13 +0700

    feat: Remove snap view and use external browser instead

--- ui/src/main/kotlin/com/midtrans/sdk/uikit/internal/presentation/ewallet/DeepLinkActivity.kt
+++ ui/src/main/kotlin/com/midtrans/sdk/uikit/internal/presentation/ewallet/DeepLinkActivity.kt
@@ -8,28 +8,14 @@ import android.net.Uri
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.Text
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.lifecycle.ViewModelProvider
 import com.midtrans.sdk.corekit.api.model.PaymentType
 import com.midtrans.sdk.corekit.api.model.TransactionResult
-import com.midtrans.sdk.corekit.core.Logger
-import com.midtrans.sdk.corekit.core.Logger.TAG
-import com.midtrans.sdk.uikit.R
 import com.midtrans.sdk.uikit.external.UiKitApi
 import com.midtrans.sdk.uikit.internal.base.BaseActivity
 import com.midtrans.sdk.uikit.internal.util.UiKitConstants
-import com.midtrans.sdk.uikit.internal.view.GifImage
-import com.midtrans.sdk.uikit.internal.view.SnapButton
-import com.midtrans.sdk.uikit.internal.view.SnapColors
-import com.midtrans.sdk.uikit.internal.view.SnapWebView
 import javax.inject.Inject
 
 internal class DeepLinkActivity : BaseActivity() {
@@ -56,7 +42,7 @@ internal class DeepLinkActivity : BaseActivity() {
         UiKitApi.getDefaultInstance().daggerComponent.inject(this)
         viewModel.setPaymentType(paymentType)
         viewModel.trackPageViewed(currentStepNumber)
-        setContent { Content(paymentType = paymentType, url = url) }
+        setContent { Content(url = url, snapToken = snapToken) }
         observeData()
     }
 
@@ -73,81 +59,24 @@ internal class DeepLinkActivity : BaseActivity() {
     }
 
     @Composable
-    private fun Content(paymentType: String, url: String) {
-        var loading by remember { mutableStateOf(true) }
-
-        Box(
-            modifier = Modifier
-                .fillMaxWidth(1f)
-                .fillMaxHeight(1f)
-        ) {
-            SnapWebView(
-                title = "",
-                paymentType = paymentType,
-                url = url,
-                onPageStarted = { loading = true },
-                onPageFinished = { loading = false },
-                urlLoadingOverride = { webview, url ->
-                    Logger.e(TAG, "urlOverload: $url")
-
-                    if (url.contains("gojek") || url.contains("shopee")) {  // TODO: fill with exact scheme
-                        try {
-                            intent = Intent(Intent.ACTION_VIEW)
-                            intent.setData(Uri.parse(url))
-                            deepLinkLauncher.launch(intent)
-                            true
-                        } catch (e: Throwable) {
-                            openAppInPlayStore()
-                            true
-                        }
-                    } else {
-                        false
-                    }
-                }
-            )
-            if (loading) {
-                Box(
-                    modifier = Modifier
-                        .fillMaxWidth(1f)
-                        .fillMaxHeight(1f)
-                        .background(color = SnapColors.getARGBColor(SnapColors.backgroundFillPrimary))
-                ) {
-                    Column(
-                        modifier = Modifier.align(Alignment.Center),
-                        horizontalAlignment = Alignment.CenterHorizontally
-                    ) {
-                        GifImage(
-                            gifResId = R.drawable.gif_loading_ios,
-                            modifier = Modifier.width(50.dp).height(50.dp)
-                        )
-                        redirectionTitle[paymentType]?.let {
-                            Text(text = stringResource(id = it))
-                        }
-                    }
-                    redirectionCta[paymentType]?.let {
-                        SnapButton(
-                            modifier = Modifier
-                                .align(Alignment.BottomCenter)
-                                .fillMaxWidth(1f)
-                                .padding(16.dp),
-                            text = stringResource(id = it),
-                            style = SnapButton.Style.TERTIARY
-                        ) {
-                            viewModel.trackSnapButtonClicked(
-                                ctaName = getStringResourceInEnglish(it),
-                                paymentType = paymentType
-                            )
-                        }
-                    }
-                }
+    private fun Content(url: String, snapToken: String) {
+        if (isValidUrl(url)) {
+            try {
+                intent = Intent(Intent.ACTION_VIEW)
+                intent.setData(Uri.parse(url))
+                deepLinkLauncher.launch(intent)
+            } catch (e: Throwable) {
+                openAppInPlayStore()
             }
+        } else {
+            openAppInPlayStore()
         }
     }
 
     @Preview
     @Composable
     private fun ForPreview() {
-        Content(paymentType = PaymentType.GOPAY, url = "http://")
+        Content(url = "http://", snapToken = "")
     }
 
     private fun getAppPackageName(): String {
@@ -173,18 +102,16 @@ internal class DeepLinkActivity : BaseActivity() {
         intent.getStringExtra(EXTRA_SNAP_TOKEN).orEmpty()
     }
 
-    private val redirectionTitle by lazy {
-        mapOf(
-            Pair(PaymentType.GOPAY, R.string.redirection_screen_gopay_main_message),
-            Pair(PaymentType.SHOPEEPAY, R.string.redirection_screen_shopeepay_main_message),
-        )
-    }
-
-    private val redirectionCta by lazy {
-        mapOf(
-            Pair(PaymentType.GOPAY, R.string.redirection_screen_gopay_cta),
-            Pair(PaymentType.SHOPEEPAY, R.string.redirection_screen_shopeepay_cta),
-        )
+    private fun isValidUrl(url: String): Boolean {
+        if (url.contains("gojek://") ||
+            url.contains("shopeeid://") ||
+            // This is handle for sandbox Simulator
+            url.contains("/gopay/partner/") ||
+            url.contains("/shopeepay/")
+        ) {  // TODO: fill with exact scheme
+            return true
+        }
+        return false
     }
 
     private fun openAppInPlayStore() {
