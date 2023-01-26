e683cb22154cd0b6873f3308d0f992fe6e7b3e36
Ferdian Julianto <Juliantoferdian24@gmail.com>
2023-01-26 11:12:17 +0700

    feat: change datatype of terms in InstallmentLegacy from List to ArrayList

--- ui/src/main/java/com/midtrans/sdk/corekit/models/snap/Installment.java
+++ ui/src/main/java/com/midtrans/sdk/corekit/models/snap/Installment.java
@@ -1,11 +1,12 @@
 package com.midtrans.sdk.corekit.models.snap;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 public class Installment {
     private boolean isRequired;
-    private Map<String, List<Integer>> terms;
+    private Map<String, ArrayList<Integer>> terms;
 
     public boolean isRequired() {
         return isRequired;
@@ -15,11 +16,11 @@ public class Installment {
         isRequired = required;
     }
 
-    public Map<String, List<Integer>> getTerms() {
+    public Map<String, ArrayList<Integer>> getTerms() {
         return terms;
     }
 
-    public void setTerms(Map<String, List<Integer>> terms) {
+    public void setTerms(Map<String, ArrayList<Integer>> terms) {
         this.terms = terms;
     }
 }
