c0aecad94b1f7fbf36a5d74e430f8e225d32b283
Ferdian Julianto <Juliantoferdian24@gmail.com>
2023-02-01 12:42:35 +0700

    feat: remove Authentication RBA

--- corekit/src/main/kotlin/com/midtrans/sdk/corekit/internal/util/Utils.kt
+++ corekit/src/main/kotlin/com/midtrans/sdk/corekit/internal/util/Utils.kt
@@ -6,8 +6,6 @@ object Utils {
     fun mappingToCreditCardAuthentication(type: String?, secure: Boolean): String? {
         return if (type.equals(Authentication.AUTH_3DS, ignoreCase = true) && secure) {
             Authentication.AUTH_3DS
-        } else if (type.equals(Authentication.AUTH_RBA, ignoreCase = true) && !secure) {
-            Authentication.AUTH_RBA
         } else {
             Authentication.AUTH_NONE
         }
