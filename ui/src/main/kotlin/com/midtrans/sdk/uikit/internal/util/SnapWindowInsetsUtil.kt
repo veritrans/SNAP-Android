15fa65e70a42990843049600c9029d641c133b64
Muhammad Fauzi Masykur <muhammad.masykur@gojek.com>
2025-09-03 14:42:42 +0700

    chore: remove unused code

--- ui/src/main/kotlin/com/midtrans/sdk/uikit/internal/util/SnapWindowInsetsUtil.kt
+++ ui/src/main/kotlin/com/midtrans/sdk/uikit/internal/util/SnapWindowInsetsUtil.kt
@@ -1,11 +1,9 @@
 package com.midtrans.sdk.uikit.internal.util
 
 import android.os.Build
-import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
-import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -14,25 +12,7 @@ import androidx.compose.ui.unit.dp
  * Handles window insets for Android 15+ edge-to-edge display
  */
 internal object SnapWindowInsetsUtil {
-    
-    @Composable
-    fun Modifier.snapNavigationBarsPadding(): Modifier {
-        return if (Build.VERSION.SDK_INT >= 35) {
-            this.navigationBarsPadding()
-        } else {
-            this
-        }
-    }
-    
-    @Composable
-    fun Modifier.snapSystemBarsPadding(): Modifier {
-        return if (Build.VERSION.SDK_INT >= 35) {
-            this.systemBarsPadding()
-        } else {
-            this
-        }
-    }
-    
+
     @Composable
     fun Modifier.snapStatusBarsPadding(): Modifier {
         return if (Build.VERSION.SDK_INT >= 35) {
@@ -41,10 +21,7 @@ internal object SnapWindowInsetsUtil {
             this
         }
     }
-    
-    /**
-     * Adds bottom padding for buttons and CTAs
-     */
+
     @Composable
     fun Modifier.snapSafeBottomPadding(): Modifier {
         return if (Build.VERSION.SDK_INT >= 35) {
@@ -55,18 +32,8 @@ internal object SnapWindowInsetsUtil {
             this.padding(bottom = 16.dp)
         }
     }
-    
-    @Composable
-    fun Modifier.snapImePadding(): Modifier {
-        return if (Build.VERSION.SDK_INT >= 35) {
-            this.imePadding()
-        } else {
-            this
-        }
-    }
 }
 
-// Extension functions for SDK's internal UI components
 @Composable
 internal fun Modifier.snapStatusBarsPadding(): Modifier = 
     with(SnapWindowInsetsUtil) { this@snapStatusBarsPadding.snapStatusBarsPadding() }
