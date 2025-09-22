595044083554dc44d60f33847397c236a87c68a1
Muhammad Fauzi Masykur <muhammad.masykur@gojek.com>
2025-09-22 14:56:32 +0700

    feat:  implement unit tests and fix unit tests not working by bumping  jvm and mockito.

--- ui/build.gradle.kts
+++ ui/build.gradle.kts
@@ -88,8 +88,8 @@ android {
 
     compileOptions {
         isCoreLibraryDesugaringEnabled = false
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
     packaging {
         resources {
@@ -107,7 +107,7 @@ android {
     }
 
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "11"
     }
     namespace = "com.midtrans.sdk.uikit"
 
